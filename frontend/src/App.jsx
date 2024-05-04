884ec41e9a5c2d908117d5551047617519cc767c
srnleapkore <sarant.srn@gmail.com>
2024-05-04 21:30:10 +0530

    Basic Routes and Page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Portal from './Pages/Portal/Portal'
+import Account from './Pages/Account/Account'
+import Login from './Pages/Login/Login'
+import Signup from './Pages/Signup/Signup'
+
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Portal/>}/>
+      <Route path="/account" element={<Account/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
