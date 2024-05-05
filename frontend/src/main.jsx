9cfe1e37c9bde5cae42486226396ab529ac989a8
srnleapkore <sarant.srn@gmail.com>
2024-05-05 17:23:14 +0530

    Redux persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {store} from './redux/store.js'
-import {Provider} from 'react-redux'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
+);
