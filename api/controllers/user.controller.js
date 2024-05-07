0f09813e9361c6d9a69709af3b9aad0ce03992ef
srnleapkore <sarant.srn@gmail.com>
2024-05-07 16:58:03 +0530

    api route for update user profile

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,35 @@
-export const sample = (req,res) =>{
-    res.json({message:'API working'});
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
+export const sample = (req, res) => {
+  res.json({ message: "API working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You cannot make changes to this user"));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 8) {
+      return next(errorHandler(400, "Password must be atleast 8 characters"));
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          password: req.body.password,
+          profilepicture: req.body.profilepicture,
+        },
+      },
+      { new: true }
+    );
+    const {password, ...rest} = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import { sample } from '../controllers/user.controller.js';
+import express from "express";
+import { sample, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get('/sample', sample);
+router.get("/sample", sample);
+router.put("/updateprofile/:userId", verifyToken, updateUser);
 
 export default router;
